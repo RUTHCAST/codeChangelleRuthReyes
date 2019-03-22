6b1bc04b3137844cc0d2ae93c3bd5e0f7104cedb
ruth <ruth_castellin@hotmail.com>
2019-03-22 04:17:16 -0400

    primer commit

---
+++ js/detalleVista.js
@@ -0,0 +1,132 @@
+
+
+function mostrar(){
+    let div=document.getElementById('uno');
+    div.style.opacity=1;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,-280px)";
+    div2.style.transition="all 2s";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,280px)";
+    div3.style.transition="all 2s";
+
+
+}
+function ocultar(){
+    let div=document.getElementById('uno');
+    div.style.opacity=0;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,280px)";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,-280px)";
+    div3.style.transition="all 2s";
+}
+
+function mostrar2(){
+    let div=document.getElementById('dos');
+    div.style.opacity=1;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,-280px)";
+    div2.style.transition="all 2s";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,280px)";
+    div3.style.transition="all 2s";
+
+
+}
+function ocultar2(){
+    let div=document.getElementById('dos');
+    div.style.opacity=0;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,280px)";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,-280px)";
+    div3.style.transition="all 2s";
+}
+function mostrar3(){
+    let div=document.getElementById('tres');
+    div.style.opacity=1;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,-280px)";
+    div2.style.transition="all 2s";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,280px)";
+    div3.style.transition="all 2s";
+
+
+}
+function ocultar3(){
+    let div=document.getElementById('tres');
+    div.style.opacity=0;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,280px)";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,-280px)";
+    div3.style.transition="all 2s";
+}
+function mostrar4(){
+    let div=document.getElementById('cuatro');
+    div.style.opacity=1;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,-280px)";
+    div2.style.transition="all 2s";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,280px)";
+    div3.style.transition="all 2s";
+
+
+}
+function ocultar4(){
+    let div=document.getElementById('cuatro');
+    div.style.opacity=0;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,280px)";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,-280px)";
+    div3.style.transition="all 2s";
+}
+function mostrar5(){
+    let div=document.getElementById('cinco');
+    div.style.opacity=1;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,-280px)";
+    div2.style.transition="all 2s";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,280px)";
+    div3.style.transition="all 2s";
+
+
+}
+function ocultar5(){
+    let div=document.getElementById('cinco');
+    div.style.opacity=0;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,280px)";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,-280px)";
+    div3.style.transition="all 2s";
+}
+function mostrar6(){
+    let div=document.getElementById('seis');
+    div.style.opacity=1;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,-280px)";
+    div2.style.transition="all 2s";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,280px)";
+    div3.style.transition="all 2s";
+
+
+}
+function ocultar6(){
+    let div=document.getElementById('seis');
+    div.style.opacity=0;
+    let div2=document.getElementById('filaSuperior');
+    div2.style.transform = "translate(0px,280px)";
+    let div3=document.getElementById('filaInferior');
+    div3.style.transform = "translate(0px,-280px)";
+    div3.style.transition="all 2s";
+}
+
+
+
